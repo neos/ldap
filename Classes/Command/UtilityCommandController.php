a8ff17580e9516b9ee4b84061aae9cc6888699bd
Christian Schwahn <c.schwahn@paints.de>
2017-10-23 15:40:01 +0200

    [TASK] create neos user and check credentials by ldap

--- Classes/Command/UtilityCommandController.php
+++ Classes/Command/UtilityCommandController.php
@@ -1,5 +1,6 @@
 <?php
-namespace Neos\Ldap\Command;
+
+namespace Neos\Ldap\Security\Authentication\Provider;
 
 /*
  * This file is part of the Neos.Ldap package.
@@ -11,224 +12,284 @@ namespace Neos\Ldap\Command;
  * source code.
  */
 
-use Symfony\Component\Yaml\Yaml;
 use Neos\Flow\Annotations as Flow;
-use Neos\Flow\Cli\CommandController;
-use Neos\Utility\Arrays;
-use Neos\Utility\Files;
+use Neos\Flow\Log\SecurityLoggerInterface;
+use Neos\Flow\Security\Account;
+use Neos\Flow\Security\Authentication\Provider\PersistedUsernamePasswordProvider as PersistedUsernamePasswordProviderOrg;
+use Neos\Flow\Security\Authentication\Token\UsernamePassword;
+use Neos\Flow\Security\Authentication\TokenInterface;
+use Neos\Flow\Security\Exception\UnsupportedAuthenticationTokenException;
+use Neos\Flow\Security\Policy\PolicyService;
 use Neos\Ldap\Service\DirectoryService;
+use Neos\Neos\Domain\Model\User;
+use Neos\Neos\Domain\Service\UserService;
+use Neos\ContentRepository\Domain\Model\Workspace;
+use Neos\ContentRepository\Domain\Repository\WorkspaceRepository;
+use Neos\Neos\Utility\User as UserUtility;
 
 /**
- * Command controller to test settings and query the directory
+ * Ldap Authentication provider
+ *
+ * @Flow\Scope("prototype")
  */
-class UtilityCommandController extends CommandController
+class PersistedUsernamePasswordProvider extends PersistedUsernamePasswordProviderOrg
 {
 
     /**
-     * @Flow\InjectConfiguration(path="security.authentication.providers", package="Neos.Flow")
+     * @Flow\Inject
+     * @var WorkspaceRepository
+     */
+    protected $workspaceRepository;
+
+    /**
+     * @Flow\Inject
+     * @var UserService
+     */
+    protected $userService;
+
+    /**
+     * @Flow\InjectConfiguration(path="roles", package="Neos.Ldap")
      * @var array
      */
-    protected $authenticationProvidersConfiguration;
+    protected $rolesConfiguration;
 
     /**
-     * @var array
+     * @Flow\Inject
+     * @var PolicyService
      */
-    protected $options;
+    protected $policyService;
 
     /**
-     * Simple bind command to test if a bind is possible at all
+     * @var DirectoryService
+     */
+    protected $directoryService;
+
+    /**
+     * @Flow\Inject
+     * @var SecurityLoggerInterface
+     */
+    protected $logger;
+
+    /**
+     * @param string $name The name of this authentication provider
+     * @param array $options Additional configuration options
+     */
+    public function __construct($name, array $options)
+    {
+        parent::__construct($name, $options);
+        $this->directoryService = new DirectoryService($name, $options);
+    }
+
+    /**
+     * Authenticate the current token. If it's not possible to connect to the LDAP server the provider
+     * tries to authenticate against cached credentials in the database that were
+     * cached on the last successful login for the user to authenticate.
      *
-     * @param string $username Username to be used while binding
-     * @param string $password Password to be used while binding
-     * @param string $providerName Name of the authentication provider to use
-     * @param string $settingsFile Path to a yaml file containing the settings to use for testing purposes
+     * @param TokenInterface $authenticationToken The token to be authenticated
+     * @throws UnsupportedAuthenticationTokenException
      * @return void
      */
-    public function bindCommand($username = null, $password = null, $providerName = null, $settingsFile = null)
+    public function authenticate(TokenInterface $authenticationToken)
     {
-        $directoryService = $this->getDirectoryService($providerName, $settingsFile);
+        if (!($authenticationToken instanceof UsernamePassword)) {
+            throw new UnsupportedAuthenticationTokenException('This provider cannot authenticate the given token.', 1217339840);
+        }
+
+        $credentials = $authenticationToken->getCredentials();
+
+        if (!is_array($credentials) || !isset($credentials['username'])) {
+            $authenticationToken->setAuthenticationStatus(TokenInterface::NO_CREDENTIALS_GIVEN);
+            return;
+        }
 
         try {
-            if ($username === null && $password === null) {
-                $result = ldap_bind($directoryService->getConnection());
-                $this->outputLine('Anonymous bind attempt %s', [$result === false ? 'failed' : 'succeeded']);
-                if ($result === false) {
-                    $this->quit(1);
-                }
-            } else {
-                $directoryService->bind($username, $password);
-                $this->outputLine('Bind successful with user %s, using password is %s', [$username, $password === null ? 'NO' : 'YES']);
+            $ldapUser = $this->directoryService->authenticate($credentials['username'], $credentials['password']);
+
+            // Retrieve or create account for the credentials
+            $account = $this->accountRepository->findActiveByAccountIdentifierAndAuthenticationProviderName($credentials['username'], 'Neos.Neos:Backend');
+            if ($account === null) {
+                $account = $this->createAccountForCredentials($credentials, $ldapUser);
+                $this->emitAccountCreated($account, $ldapUser);
             }
+
+            // Map security roles to account
+            $this->setRoles($account, $ldapUser);
+            $this->emitRolesSet($account, $ldapUser);
+
+            // Mark authentication successful
+            $authenticationToken->setAuthenticationStatus(TokenInterface::AUTHENTICATION_SUCCESSFUL);
+            $authenticationToken->setAccount($account);
+            $this->emitAccountAuthenticated($account, $ldapUser);
+
         } catch (\Exception $exception) {
-            $this->outputLine('Failed to bind with username %s, using password is %s', [$username, $password === null ? 'NO' : 'YES']);
-            $this->outputLine($exception->getMessage());
-            $this->quit(1);
+            $this->logger->log('Authentication failed: ' . $exception->getMessage(), LOG_ALERT);
+            $authenticationToken->setAuthenticationStatus(TokenInterface::WRONG_CREDENTIALS);
         }
     }
 
     /**
-     * Try authenticating a user using a DirectoryService that's connected to a directory
+     * Creates a personal workspace for the given user's account if it does not exist already.
      *
-     * @param string $username The username to authenticate
-     * @param string $password The password to use while authenticating
-     * @param string $providerName Name of the authentication provider to use
-     * @param string $settingsFile Path to a yaml file containing the settings to use for testing purposes
+     * @param User $user The new user to create a workspace for
+     * @param Account $account The user's backend account
+     * @throws IllegalObjectTypeException
+     */
+    protected function createPersonalWorkspace(User $user, Account $account)
+    {
+        $userWorkspaceName = UserUtility::getPersonalWorkspaceNameForUsername($account->getAccountIdentifier());
+        $userWorkspace = $this->workspaceRepository->findByIdentifier($userWorkspaceName);
+        if ($userWorkspace === null) {
+            $liveWorkspace = $this->workspaceRepository->findByIdentifier('live');
+            if (!($liveWorkspace instanceof Workspace)) {
+                $liveWorkspace = new Workspace('live');
+                $liveWorkspace->setTitle('Live');
+                $this->workspaceRepository->add($liveWorkspace);
+            }
+
+            $userWorkspace = new Workspace($userWorkspaceName, $liveWorkspace, $user);
+            $userWorkspace->setTitle((string)$user->getName());
+            $this->workspaceRepository->add($userWorkspace);
+        }
+    }
+
+    /**
+     * Create a new account for the given credentials. Return null if you
+     * do not want to create a new account, that is, only authenticate
+     * existing accounts from the database and fail on new logins.
+     *
+     * @param array $credentials array containing username and password
+     * @return Account
+     */
+    protected function createAccountForCredentials(array $credentials, array $ldapUser)
+    {
+        $user = $this->userService->getUser($credentials['username'], 'Neos.Neos:Backend');
+        if ($user) {
+            //update password
+            $this->userService->setUserPassword($user, $credentials['password']);
+            $account = $this->accountRepository->findByAccountIdentifierAndAuthenticationProviderName($credentials['username'], 'Neos.Neos:Backend');
+        } else {
+            //add user
+            $user = $this->userService->createUser(
+                $credentials['username'],
+                $credentials['password'],
+                $ldapUser['givenname'][0],
+                $ldapUser['sn'][0],
+                $this->rolesConfiguration['default'],
+                'Neos.Neos:Backend'
+            );
+            $this->persistenceManager->persistAll();
+
+            //create workspace
+            $account = $this->accountRepository->findByAccountIdentifierAndAuthenticationProviderName($credentials['username'], 'Neos.Neos:Backend');
+            $this->createPersonalWorkspace($user, $account);
+        }
+        // Set user deactivated to prevent login check from 'Neos.Neos:Backend' Provider for next login
+        // so the credentials can be checked for each login by the Ldap connector
+        $this->userService->deactivateUser($user);
+
+        return $account;
+    }
+
+    /**
+     * Sets the roles for the Ldap account.
+     * Extend this Provider class and implement this method to update the party
+     *
+     * @param Account $account
+     * @param array $ldapSearchResult
      * @return void
      */
-    public function authenticateCommand($username, $password, $providerName = null, $settingsFile = null)
+    protected function setRoles(Account $account, array $ldapSearchResult)
     {
-        $directoryService = $this->getDirectoryService($providerName, $settingsFile);
+        $this->setDefaultRoles($account);
+        $this->setRolesMappedToUserDn($account, $ldapSearchResult);
+        $this->setRolesBasedOnGroupMembership($account, $ldapSearchResult);
 
-        try {
-            $directoryService->authenticate($username, $password);
-            $this->outputLine('Successfully authenticated %s with given password', [$username]);
-        } catch (\Exception $exception) {
-            $this->outputLine($exception->getMessage());
-            $this->quit(1);
+        $this->accountRepository->update($account);
+    }
+
+    /**
+     * Set all default roles
+     *
+     * @param Account $account
+     */
+    protected function setDefaultRoles(Account $account)
+    {
+        if (!is_array($this->rolesConfiguration['default'])) {
+            return;
+        }
+
+        foreach ($this->rolesConfiguration['default'] as $roleIdentifier) {
+            $account->addRole($this->policyService->getRole($roleIdentifier));
         }
     }
 
     /**
-     * Query the directory
+     * Map configured roles based on user dn
      *
-     * @param string $query The query to use, for example (objectclass=*)
-     * @param string $baseDn The base dn to search in
-     * @param string $providerName Name of the authentication provider to use
-     * @param string $settingsFile Path to a yaml file containing the settings to use for testing purposes
-     * @param string $displayColumns Comma separated list of columns to show, like:  dn,objectclass
+     * @param Account $account
+     * @param array $ldapSearchResult
+     */
+    protected function setRolesMappedToUserDn(Account $account, array $ldapSearchResult)
+    {
+        if (!is_array($this->rolesConfiguration['userMapping'])) {
+            return;
+        }
+
+        foreach ($this->rolesConfiguration['userMapping'] as $roleIdentifier => $userDns) {
+            if (in_array($ldapSearchResult['dn'], $userDns)) {
+                $account->addRole($this->policyService->getRole($roleIdentifier));
+            }
+        }
+    }
+
+    /**
+     * Map configured roles based on group membership
+     *
+     * @param Account $account
+     * @param array $ldapSearchResult
+     */
+    protected function setRolesBasedOnGroupMembership(Account $account, array $ldapSearchResult)
+    {
+        if (!is_array($this->rolesConfiguration['groupMapping'])) {
+            return;
+        }
+
+        $memberOf = $this->directoryService->getMemberOf($ldapSearchResult['dn']);
+        foreach ($this->rolesConfiguration['groupMapping'] as $roleIdentifier => $groupDns) {
+            if (!empty(array_intersect($memberOf, $groupDns))) {
+                $account->addRole($this->policyService->getRole($roleIdentifier));
+            }
+        }
+    }
+
+    /**
+     * @param Account $account
+     * @param array $ldapSearchResult
      * @return void
+     * @Flow\Signal
      */
-    public function queryCommand(
-        $query,
-        $baseDn = null,
-        $providerName = null,
-        $settingsFile = null,
-        $displayColumns = 'dn'
-    ) {
-        $directoryService = $this->getDirectoryService($providerName, $settingsFile);
-
-        if ($baseDn === null) {
-            $baseDn = Arrays::getValueByPath($this->options, 'baseDn');
-        }
-
-        $this->outputLine('Query: %s', [$query]);
-        $this->outputLine('Base DN: %s', [$baseDn]);
-
-        $searchResult = @ldap_search(
-            $directoryService->getConnection(),
-            $baseDn,
-            $query
-        );
-
-        if ($searchResult === false) {
-            $this->outputLine(ldap_error($directoryService->getConnection()));
-            $this->quit(1);
-        }
-
-        $this->outputLdapSearchResultTable($directoryService->getConnection(), $searchResult, $displayColumns);
-    }
-
-    /**
-     * @param string $providerName Name of the authentication provider to use
-     * @param string $settingsFile Path to a yaml file containing the settings to use for testing purposes
-     * @return DirectoryService
-     * @throws \Neos\Flow\Mvc\Exception\StopActionException
-     */
-    protected function getDirectoryService($providerName, $settingsFile)
+    public function emitAccountCreated(Account $account, array $ldapSearchResult)
     {
-        $directoryServiceOptions = $this->getOptions($providerName, $settingsFile);
-        if (!is_array($directoryServiceOptions)) {
-            $this->outputLine('No configuration found for given providerName / settingsFile');
-            $this->quit(3);
-        }
-
-        return new DirectoryService('cli', $directoryServiceOptions);
     }
 
     /**
-     * Load options by provider name or by a settings file (first has precedence)
-     *
-     * @param string $providerName Name of the authentication provider to use
-     * @param string $settingsFile Path to a yaml file containing the settings to use for testing purposes
-     * @return array|mixed
-     * @throws \Neos\Flow\Mvc\Exception\StopActionException
-     */
-    protected function getOptions($providerName = null, $settingsFile = null)
-    {
-        if ($providerName !== null && array_key_exists($providerName, $this->authenticationProvidersConfiguration)) {
-            $this->options = $this->authenticationProvidersConfiguration[$providerName]['providerOptions'];
-            return $this->options;
-        }
-
-        if ($settingsFile !== null) {
-            if (!file_exists($settingsFile)) {
-                $this->outputLine('Could not find settings file on path %s', [$settingsFile]);
-                $this->quit(1);
-            }
-            $this->options = Yaml::parse(Files::getFileContents($settingsFile));
-            return $this->options;
-        }
-
-        $this->outputLine('Neither providerName or settingsFile is passed as argument. You need to pass one of those.');
-        $this->quit(1);
-    }
-
-    /**
-     * Outputs a table for given search result
-     *
-     * @param resource $connection
-     * @param resource $searchResult
-     * @param $displayColumns
+     * @param Account $account
+     * @param array $ldapSearchResult
      * @return void
+     * @Flow\Signal
      */
-    protected function outputLdapSearchResultTable($connection, $searchResult, $displayColumns)
+    public function emitAccountAuthenticated(Account $account, array $ldapSearchResult)
     {
-        $headers = [];
-        $rows = [];
-
-        $displayColumns = Arrays::trimExplode(',', $displayColumns);
-
-        $entries = ldap_get_entries($connection, $searchResult);
-        $this->outputLine('%s results found', [$entries['count']]);
-
-        foreach ($entries as $index => $ldapSearchResult) {
-            if ($index === 'count') {
-                continue;
-            }
-
-            if ($headers === []) {
-                foreach ($ldapSearchResult as $propertyName => $propertyValue) {
-                    if (is_integer($propertyName)) {
-                        continue;
-                    }
-                    if ($displayColumns === null || in_array($propertyName, $displayColumns)) {
-                        $headers[] = $propertyName;
-                    }
-                }
-            }
-
-            $row = [];
-            foreach ($ldapSearchResult as $propertyName => $propertyValue) {
-                if (is_integer($propertyName)) {
-                    continue;
-                }
-                if ($displayColumns !== null && !in_array($propertyName, $displayColumns)) {
-                    continue;
-                }
-
-                if (isset($propertyValue['count'])) {
-                    unset($propertyValue['count']);
-                }
-
-                if (is_array($propertyValue)) {
-                    $row[$propertyName] = implode(", ", $propertyValue);
-                } else {
-                    $row[$propertyName] = $propertyValue;
-                }
-            }
-            $rows[] = $row;
-        }
-
-        $this->output->outputTable($rows, $headers);
     }
+
+    /**
+     * @param Account $account
+     * @param array $ldapSearchResult
+     * @return void
+     * @Flow\Signal
+     */
+    public function emitRolesSet(Account $account, array $ldapSearchResult)
+    {
+    }
+
 }
